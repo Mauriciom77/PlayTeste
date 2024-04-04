b2fa4bc109ad5f8ff59c573ada437f460075d91e
Mauricio <mauricio@unicorp.com.br>
2024-04-04 17:31:58 -0300

    add teste

---
+++ tests/google.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect, type Page } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://google.com.br');
+});
+
+const TODO_ITEMS = [
+  'buy some cheese',
+  'feed the cat',
+  'book a doctors appointment'
+];
+
+
+test('should allow me to add todo items', async ({ page }) => {
+    await page.screenshot({ path: 'screenshot.png' });
+
+});
+    
+  
